d1f1ed61456f7fbe6fdbd8db3a6b2b9d8b2118a8
Reggiezu <reginaldosakue@gmail.com>
2025-11-04 16:19:52 -0500

    skeleton Git repository for 'Odin Project' Project: Library

---
+++ main.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book() {
+    this.id= crypto.randomUUID();
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+console.log
